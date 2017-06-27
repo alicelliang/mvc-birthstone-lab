da6a832d7842d61184051b79b5a6b9251e88f94f
Alice Liang <aliceliang011@gmail.com>
2017-06-27 23:21:39 +0000

    unfinished

--- application_controller.rb
+++ application_controller.rb
@@ -8,6 +8,16 @@ class MyApp < Sinatra::Base
     erb :index
   end
 
+  post '/birthstone_match' do
+    user_input=params[:user_birthmonth]
+    @answer=choose_gem(user_input)
+    erb :results
+  end
   
-  
+  post '/birthstone_picture' do
+    user_input=params[:user_birthmonth]
+    @picture=birthstone_pic(user_input)
+    erb :results
+  end
+
 end
--- models/model.rb
+++ models/model.rb
@@ -1,5 +1,36 @@
 def choose_gem(month)
+    birthstone={
+    :january => "Garnet",
+    :february => "Amethyst",
+    :march => "Aquamarine, Bloodstone",
+    :april => "Diamond",
+    :may => "Emerald",
+    :june => "Pearl, Alexandrite, Moonstone",
+    :july => "Ruby",
+    :august => "Peridot",
+    :september => "Sapphire",
+    :october => "Tourmeline",
+    :november => "Citrine, Yellow Topaz",
+    :december => "Ziron, Turquoise"
+}
+    birthstone[:"#{month.downcase}"]
+end
 
- 
+def birthstone_pic(month)
+     birthstone_hash={
+    :january => "images/gamet.jpg",
+    :february => "images/amethyst.jpg",
+    :march => "images/aquamarine.jpg",
+    :april => "images/diamond.png",
+    :may => "images/emerald.jpg",
+    :june => "images/pearl.jpg",
+    :july => "images/ruby.jpg",
+    :august => "images/peridot.jpg",
+    :september => "images/sapphire.jpg",
+    :october => "images/tourmeline.jpg",
+    :november => "images/citrine.jpg",
+    :december => "images/turquoise.jpg"
+     }
+     birthstone_hash[:"#{month.downcase}"] 
 end
 
